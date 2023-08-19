8bdb04fe8782b2f9cab4e386ad7fbb7cbe860c5f
sergiy <s.n.cherep@gmail.com>
2023-08-19 16:16:37 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const grid = document.querySelector('.inner__wrapper_sticker');
-  const stickerMain = document.querySelector('.sticker__card.card-add');
+  const stickerMain = document.querySelector('.sticker__card-plus.card-add');
 
   stickerMain.addEventListener('click', function () {
     const sticker = document.createElement('div');
     sticker.classList.add('sticker__card');
     sticker.setAttribute('contenteditable', 'true');
     grid.insertBefore(sticker, this);
+
+    const stickerNew = document.querySelector('.sticker__card');
+    const windowNewText = document.querySelector('.sticker__window_section');
+    stickerNew.addEventListener('click', function () {
+      windowNewText.classList.remove('hidden');
+    });
   });
 });

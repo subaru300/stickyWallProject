d432bfb8a072ccad0150c1929c4edd7d8bcdbde5
Nazar <nazarobeziuk@gmail.com>
2023-08-20 09:34:28 +0200

    I added some classes to js code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 'use strict';
 
+// Очікування завантаження контенту сторінки
 document.addEventListener('DOMContentLoaded', function () {
+  // Вибірка необхідних DOM-елементів
   const grid = document.querySelector('.inner__wrapper_sticker');
   const stickerMain = document.querySelector('.sticker__card.card-add');
   const addButton = document.querySelector('.window__btn_add');
 
+  // Лічильник для створення унікальних ідентифікаторів для стікерів
   let counter = 0;
 
+  // Додавання події "click" на головний стікер
   stickerMain.addEventListener('click', function () {
+    // Створення нового елементу стікера
     const sticker = document.createElement('div');
     sticker.classList.add('sticker__card');
     sticker.id = `sticker${counter}`;
     sticker.setAttribute('contenteditable', 'true');
     grid.insertBefore(sticker, this);
 
-    ///// adding text in sticker
-
+    // Додавання події "click" на кнопку додавання тексту в стікер
     addButton.addEventListener('click', function () {
+      // Отримання стікера за його ідентифікатором
       let stickerTextId = document.getElementById(`sticker${counter}`);
+      // Заповнення вмістом з вікна тексту
       stickerTextId.textContent = windowTextTitle.textContent;
       stickerTextId.textContent = windowTextDescription.textContent;
     });
@@ -25,6 +31,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// Вибірка необхідних DOM-елементів поза "DOMContentLoaded"
 const windowTextTitle = document.querySelector('.window__text_title');
 const windowTextDescription = document.querySelector(
   '.window__text_description'
@@ -32,30 +39,37 @@ const windowTextDescription = document.querySelector(
 const stickerNew = document.querySelector('.card-add');
 const windowNewText = document.querySelector('.sticker__window_section');
 
+// Додавання події "click" на головний стікер для відкриття вікна
 stickerNew.addEventListener('click', function () {
+  // Активування режиму редагування для текстових полів
   windowTextTitle.setAttribute('contenteditable', 'true');
   windowTextDescription.setAttribute('contenteditable', 'true');
+  // Показ вікна для вводу тексту
   windowNewText.classList.remove('hidden');
   overlay.classList.remove('hidden');
 });
 
+// Вибірка необхідних DOM-елементів для закриття вікна
 const overlay = document.querySelector('.overlay');
 overlay.addEventListener('click', function () {
+  // Закриття вікна
   const windowNewText = document.querySelector('.sticker__window_section');
   overlay.classList.add('hidden');
   windowNewText.classList.add('hidden');
 });
 
+// Вибірка необхідних DOM-елементів для скасування введення тексту
 const stickyWindow = document.querySelector('.sticker__window_section');
 const windowCancelButton = document.querySelector('.window__btn_cancel');
 
+// Додавання події "click" на кнопку скасування для закриття вікна
 windowCancelButton.addEventListener('click', function () {
   stickyWindow.classList.add('hidden');
   windowNewText.classList.add('hidden');
   overlay.classList.add('hidden');
 });
 
-///// clean text on input window
+// Функція для очищення тексту в текстових полях
 const removeText = function (e) {
   windowTextTitle.textContent
     ? (windowTextDescription.textContent = 'Write your text')
@@ -63,6 +77,8 @@ const removeText = function (e) {
 
   e.textContent = '';
 };
+
+// Додавання подій "click" на поля вводу тексту для очищення вмісту
 windowTextTitle.addEventListener('click', function () {
   removeText(windowTextTitle);
 });
@@ -70,8 +86,8 @@ windowTextDescription.addEventListener('click', function () {
   removeText(windowTextDescription);
 });
 
-///// adding text in sticker
-
+// Додавання події "click" на кнопку додавання тексту в стікер
 addButton.addEventListener('click', function () {
+  // Заповнення заголовку стікера змістом поля вводу
   windowTextTitle.textContent = windowTextTitle.value;
 });
